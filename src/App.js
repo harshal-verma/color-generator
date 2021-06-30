98ef7b74df699ca8e88e7959b0f6764a3865480d
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-07-01 00:52:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState , useEffect } from 'react';
+import Values from 'values.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [color , setColor] = useState("");
+  const [error , setError] = useState(false);
+  const [list , setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+      let colors = new Values(color).all(10);
+      setList(colors);
+    }catch(error){
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  return <React.Fragment>
+    <section className="container">
+      <h2>color generator</h2>
+      <form onSubmit={handleSubmit}>
+         <input type="text" value={color} onChange={(e) => setColor(e.target.value)}/>
+      </form>
+      <button type="submit">generate</button>
+    </section>
+    <section>
+    </section>
+  </React.Fragment>;
 }
 
 export default App;
